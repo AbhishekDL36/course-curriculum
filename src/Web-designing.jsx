21dabb49d1b49e20733d78083029d0686811305e
Abhishek <abhishekofficials12@gmail.com>
2025-02-20 18:33:18 +0530

    setting routes using react-router-dom

---
+++ src/Web-designing.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const WebDesigning = () => {
+  return (
+    <div className="page-container">
+      <h1>Web Designing</h1>
+      <p>Learn about creating visually stunning and responsive web pages.</p>
+    </div>
+  );
+};
+
+export default WebDesigning;
