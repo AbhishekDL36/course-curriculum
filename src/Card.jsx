8306a259f69c1103315730af380bf5fa965fed87
Ishita <ishitajain385@gmail.com>
2025-02-22 17:28:18 +0530

    enabled routing via card

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,14 +1,16 @@
-/* eslint-disable react/prop-types */
+
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 
+import { Link } from 'react-router-dom';
 
-const Card = ({ title, description}) => {
+// eslint-disable-next-line react/prop-types
+const Card = ({ title, description, name }) => {
   return (
     <div className="card">
       <h3>{title}</h3>
       <p>{description}</p>
-       <a  className="cta-button">Learn More</a>  
+    <Link to={name}><a  className="cta-button">Learn More</a>   </Link>   
     
     </div>
   );
